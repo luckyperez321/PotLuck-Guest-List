76cd8020d25c65391e046562489b69340b5d64ac
Lucky Perez <luckyperez321@gmail.com>
2024-01-09 15:50:24 -0600

    finished! maybe make it an errand list

--- js/script.js
+++ js/script.js
@@ -12,18 +12,33 @@ const guestCount = document.querySelector(".attendance");
 const guestFull = document.querySelector(".alert");
 
 
-addGuestButton.addEventListener (`click`, function() {
-const guest = guestInput.value; 
-console.log(guest)
-if (guest !== "") {
-const listItem = document.createElement ('li');
-listItem.innerText = guest;
-guestList.append(listItem);
-clearInput();
-};
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    //console.log(guest)
+    if (guest !== "") {
+        addToList(guest);
+        clearInput();
+        updateGuestCount();
+    }
 });
 
-const clearInput = function(){
-guestInput.value = "";
+const clearInput = function () {
+    guestInput.value = "";
 };
 
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+const guests = document.querySelectorAll(".guest-list li")
+guestCount.innerText = guests.length;
+if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+}
+};
